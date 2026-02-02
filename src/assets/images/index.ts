0fb678ac474791b33bf17e3ba64ecc5c0203b536
surjeetIqsetters <surjeet.iqsetters@gmail.com>
2026-02-02 16:35:48 +0530

    v2

--- src/assets/images/index.ts
+++ src/assets/images/index.ts
@@ -1,7 +1,8 @@
 // Placeholder for onboarding images
 // In a real project, add actual image files here
+// Currently using null exports as images are not yet available
 
-export const onboarding1 = require('./onboarding1.png');
-export const onboarding2 = require('./onboarding2.png');
-export const onboarding3 = require('./onboarding3.png');
-export const onboarding4 = require('./onboarding4.png');
+export const onboarding1 = null;
+export const onboarding2 = null;
+export const onboarding3 = null;
+export const onboarding4 = null;
